f4d3fe5718c9b3026c2b67d8104b084053d30048
KaustubhKanagalekar <kkanagalekar@ucsd.edu>
2024-10-01 23:15:29 +0000

    initial commit

---
+++ week_2_files/turtlebot_sim/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'turtlebot_sim'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='ubuntu',
+    maintainer_email='ubuntu@todo.todo',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'teleport = turtlebot_sim.turtle:main'
+        ],
+    },
+)
