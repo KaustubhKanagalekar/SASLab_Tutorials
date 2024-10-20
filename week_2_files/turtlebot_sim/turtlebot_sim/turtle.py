0a024994a58f9558827e3720409558ae2b846800
KaustubhKanagalekar <kkanagalekar@ucsd.edu>
2024-10-20 18:21:38 +0000

    edited turtlebot sim script

--- week_2_files/turtlebot_sim/turtlebot_sim/turtle.py
+++ week_2_files/turtlebot_sim/turtlebot_sim/turtle.py
@@ -1,34 +1,103 @@
-# File: teleport_turtlebot_node.py
 import rclpy
 from rclpy.node import Node
-import random
 from geometry_msgs.msg import Twist
+from nav_msgs.msg import Odometry
+import random
+import math
+import time
+import tf_transformations
 
-class TeleportTurtleBotNode(Node):
+
+class TurtleBotMover(Node):
 
     def __init__(self):
-        super().__init__('teleport_turtlebot_node')
+        super().__init__('turtlebot_mover')
         self.publisher = self.create_publisher(Twist, '/cmd_vel', 10)
-        self.timer_period = 10.0  # Time interval to teleport the TurtleBot
-        self.timer = self.create_timer(self.timer_period, self.timer_callback)
+        self.subscriber = self.create_subscription(Odometry, '/odom', self.odometry, 10)
+        self.time_to_move = self.create_timer(0.1, self.move)
+        self.current_pose = None
+        self.goal()
 
-    def timer_callback(self):
-        self.get_logger().info('Triggering TurtleBot teleport...')
-        self.teleport_turtlebot()
+        self.start_time = self.get_clock().now().seconds_nanoseconds()[0]
+        self.distance_threshold = 0.1
+        self.constant_linear_speed = 0.7
+        self.max_angular_speed = 1.0
 
-    def teleport_turtlebot(self):
-        # Create a Twist message
-        twist_msg = Twist()
-        twist_msg.linear.x = random.uniform(-1.0, 1.0)  # Random linear velocity
-        twist_msg.angular.z = random.uniform(-1.0, 1.0)  # Random angular velocity
+    def odometry(self,msg):
+        position = msg.pose.pose.position
+        orientation = msg.pose.pose.orientation 
 
-        self.publisher.publish(twist_msg)
-        self.get_logger().info('Published velocity: linear.x: %.2f, angular.z: %.2f' % (twist_msg.linear.x, twist_msg.angular.z))
+        roll, pitch, yaw = tf_transformations.euler_from_quaternion(
+            [orientation.x, orientation.y, orientation.z, orientation.w]
+        )
+        
+        self.current_pose = (position.x, position.y, yaw)
 
-def main(args=None):
-    rclpy.init(args=args)
-    node = TeleportTurtleBotNode()
+    
+    def goal(self):
+        self.x_loc = random.uniform(-3.0,3.0)
+        self.y_loc = random.uniform(-3.0,3.0)
+        self.start_time = self.get_clock().now().seconds_nanoseconds()[0]
+        self.get_logger().info(f'New target: ({self.x_loc:.2f}, {self.y_loc:.2f})')
+
+    
+    def angle_change(self, angle):
+        while angle > math.pi:
+            angle -= 2.0 * math.pi 
+        while angle < -math.pi:
+            angle += 2.0*math.pi 
+        return angle 
+
+    def move(self):
+        if self.current_pose is None:
+            return 
+
+        curx, cury, curth = self.current_pose 
+        dx = self.x_loc - curx 
+        dy = self.y_loc - cury 
+        distance = math.sqrt(dx **2 + dy **2)
+        target_angle = math.atan2(dy,dx)
+
+        angular_err = self.angle_change(target_angle - curth)
+        angular_speed = max(min(angular_err * 2.0, self.max_angular_speed), -self.max_angular_speed)
+
+        if distance > self.distance_threshold:
+            linear_speed = self.constant_linear_speed
+        else:
+            linear_speed = 0.0
+
+        twist = Twist()
+        twist.linear.x = linear_speed 
+        twist.angular.z = angular_speed 
+        
+        self.publisher.publish(twist)
+
+        if distance < self.distance_threshold: 
+            current_time = self.get_clock().now().seconds_nanoseconds()[0]
+            elapsed_time = current_time - self.start_time 
+
+            if elapsed_time >= 10.0:
+                self.get_logger().info("Waiting")
+                self.wait(1)
+
+            else:
+                self.get_logger().info("Waiting")
+                self.wait(10.0 - elapsed_time)
+
+    
+    def wait(self, wait_t):
+        stop = Twist()
+        self.publisher.publish(stop)
+
+        time.sleep(wait_t)
+        self.goal()
+
+
+def main(args= None):
+    rclpy.init(args = args)
+    node = TurtleBotMover()
     rclpy.spin(node)
+    node.destroy_node()
     rclpy.shutdown()
 
 if __name__ == '__main__':
